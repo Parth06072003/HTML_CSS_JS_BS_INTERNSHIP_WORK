78995ac9c2230583590faaad2ad55db339a6e36e
Parth06072003 <parthpatel06072004@gmail.com>
2024-07-31 12:53:23 +0530

    first commit

---
+++ HTML_CSS_JS_BS/contactus.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    let users={
+        
+    }
+   $(".save").on("click",function () {
+        
+        // if($("#firstName").val()==null || $("#lastName").val()==null || $("#email").val()==null || $("#phone-number").val()==null){
+        //     alert("Fill All the details");
+        // } 
+});
+   $(".cancel").click(function () { 
+    alert("Data Cancel");    
+});
+   $(".view").click(function () { 
+    });
+
+});
